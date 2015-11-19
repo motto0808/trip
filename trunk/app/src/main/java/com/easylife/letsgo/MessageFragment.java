c6cab641197f23129bfb11a8a4cc2f43c781f3b9
sledge2015 <sunest@163.com>
2015-11-19 19:00:02 +0800

    测试使用

--- trunk/app/src/main/java/com/easylife/letsgo/MessageFragment.java
+++ trunk/app/src/main/java/com/easylife/letsgo/MessageFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 /**
@@ -52,7 +53,7 @@ public class MessageFragment extends Fragment {
 
         }
     }
-
+    private RadioGroup mRadioGroup;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
